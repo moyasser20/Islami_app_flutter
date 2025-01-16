8f0ca21ee302756d263939df8a35a5a94dd96f4b
moyasser20 <mohamedyasser192023@gmail.com>
2025-01-16 23:22:24 +0200

    first part in surah view details - fadl el details el surah

--- lib/features/layout/pages/quran/quran_details_view.dart
+++ lib/features/layout/pages/quran/quran_details_view.dart
@@ -1,22 +1,32 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:islamiapp/core/constants/app_assets.dart';
 import 'package:islamiapp/core/theme/app_colors.dart';
 import 'package:islamiapp/features/layout/pages/quran/widgets/SurahData.dart';
 
-class quranDetailView extends StatelessWidget {
+class quranDetailView extends StatefulWidget {
 
   static const String routeName = "QuranDetailsView";
 
-  final SurahData surahData;
 
-  const quranDetailView({super.key, required this.surahData});
+   const quranDetailView({super.key});
+
+  @override
+  State<quranDetailView> createState() => _quranDetailViewState();
+}
+
+class _quranDetailViewState extends State<quranDetailView> {
+  List<String> versesList = [];
 
   @override
   Widget build(BuildContext context) {
 
     var args = ModalRoute.of(context)?.settings.arguments as SurahData;
+
+
+    if(versesList.isEmpty) loadData(args.id.toString());
 
     return Container(
       decoration: BoxDecoration(
@@ -49,24 +59,33 @@ class quranDetailView extends StatelessWidget {
                   color: AppColor.primaryColor
               ),),
             ),
-            Text('''ابِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ
-            الْحَمْدُ لِلَّهِ رَبِّ الْعَالَمِينَ
-            الرَّحْمَنِ الرَّحِيمِ
-            مَالِكِ يَوْمِ الدِّينِ
-            إِيَّاكَ نَعْبُدُ وَإِيَّاكَ نَسْتَعِينُ
-            اهْدِنَا الصِّرَاطَ الْمُسْتَقِيمَ
-            صِرَاطَ الَّذِينَ أَنْعَمْتَ عَلَيْهِمْ غَيْرِ الْمَغْضُوبِ عَلَيْهِمْ وَلَا الضَّالِّين
+            Expanded(
+              child: ListView.builder(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                itemBuilder: (context, index) =>
+                  Text(versesList[index],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Janna",
+                    color: AppColor.primaryColor
+                ),
+                  ),
+                itemCount: versesList.length,
+              ),
+            )
 
-              ''',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontSize: 20,
-                  fontFamily: "Janna",
-                  color: AppColor.primaryColor
-              ),),
           ],
         ),
       ),
     );
+  }
+
+  void loadData(String surahId) async{
+    String content = await rootBundle.loadString("assets/files/$surahId.txt");
+    setState(() {
+      versesList = content.split("\n");
+    });
   }
 }
